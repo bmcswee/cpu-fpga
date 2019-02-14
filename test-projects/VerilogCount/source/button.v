e99cbc04bbb61315de7354ed3ccff3351ed9fc2f
Brendan McSweeney <bmcswee@gmail.com>
2019-02-14 17:17:18 -0500

    working debounced counter wooooooo

--- test-projects/VerilogCount/source/button.v
+++ test-projects/VerilogCount/source/button.v
@@ -6,17 +6,26 @@ module button (
   
   reg [7:0] counter = 8'b0;
   assign out = counter;
+  
+  reg oldRst, rstPressed;
 
   /* Combinational Logic */
   always @* begin
     if (rst) begin
       //counter[0] = ~counter[0];
     end
+    
+    if (rst&&!oldRst) begin
+      rstPressed = 1;
+    end else begin
+      rstPressed = 0;
+    end
   end
   
   /* Sequential Logic */
   always @(posedge clk) begin
-    if (rst) begin
+    oldRst<=rst;
+    if (rstPressed) begin
       // Add flip-flop reset values here
       counter = counter + 1;
     end else begin
