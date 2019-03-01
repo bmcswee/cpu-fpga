d4143c94c21781710c69260e030958baffc1fd7e
Brendan McSweeney <bmcswee@gmail.com>
2019-03-01 14:45:19 -0500

    working Xilinx testbench for ExternalIOTest

--- test-projects/ExternalIOTest/source/mojo_top_tb.v
+++ test-projects/ExternalIOTest/source/mojo_top_tb.v
@@ -1,26 +1,56 @@
-module mojo_top_tb ();
+`timescale 1ns / 1ps
 
-  // designed for http://www.techep.csi.cuny.edu/~zhangs/v.html
-  // note that extra module inputs like clock need to be removed for this to work
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   14:30:10 03/01/2019
+// Design Name:   mojo_top
+// Module Name:   C:/Users/Brendan/Desktop/school/xilinx/mojo-base-project/mojo_top_tb.v
+// Project Name:  Mojo-Base
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: mojo_top
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
 
-  reg [7:0] dip_switches;
-  wire [7:0] leds;
-  
-  mojo_top dut (
-    .trainer_dip(dip_switches),
-    .led(leds)
-  );
-  
-  initial  begin
-    $display("\t\t time,\t dip_switches,\t leds"); 
-    $monitor("%d, \t%d, \t%d",$time, dip_switches, leds); 
-  end 
-  
-  initial begin
-    dip_switches = 8'b00000000;
-    #5 dip_switches = 8'b00001111;
-    #5 dip_switches = 8'b10001111;
-    #10 $stop;
-  end
-  
+// MAKE SURE TO REMOVE ALL OTHER INPUTS FROM MAIN MODULE BEFORE RUNNING THIS TEST IN XILINX
+
+module mojo_top_tb;
+
+	// Inputs
+	reg [7:0] trainer_dip;
+
+	// Outputs
+	wire [7:0] led;
+
+	// Instantiate the Unit Under Test (UUT)
+	mojo_top uut (
+    .trainer_dip(trainer_dip),
+    .led(led)
+	);
+
+	initial begin
+		// Initialize Inputs
+		trainer_dip = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+		#50 trainer_dip = 8'b00000001;
+		#50 trainer_dip = 8'b00001111;
+		#50 trainer_dip = 8'b10001111;
+		#50 $stop;
+	end
+      
 endmodule
+
